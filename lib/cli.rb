cf85d8f8b6ffdd3c8617bebc50098b6466722b66
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-28 19:28:09 -0400

    added real data to db

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,9 @@
 class CLI
 
-
     def run
+        pid = fork{ exec 'afplay', "/Users/jeanmariejackman/Development/code/Mod1/project/ruby-project-guidelines-nyc01-seng-ft-051120/lib/POL-reach-the-sky-short.wav" }
         self.greeting 
         self.create_new_player
-
         while true
             # puts "\nType 'info' to get a list of options. Say 'quit' to exit game."
             sleep(0.25)
@@ -23,6 +22,8 @@ class CLI
                 self.inspect_player
             when "delete"
                 self.delete_player
+            when "create"
+                self.create_new_player
             when "go"
                 self.enter_game
         else
@@ -32,7 +33,55 @@ class CLI
 end  
 
     def greeting
-        puts "Welcome to Cavern of the Ice Wizard"
+        system "clear"
+        puts ""                                                                                                                     
+        puts "     _____                                    __   _   _            _____          __          ___                  _ "
+        puts "    / ____|                                  / _| | | | |          |_   _|         X X        / (_)                | |"
+        sleep(0.25)
+        puts "   | |     __ ___   _____ _ __ _ __     ___ | |_  | |_| |__   ___    | |  ___ ___   X X  /X  / / _ ______ _ _ __ __| |"
+        sleep(0.25)
+        puts "   | |    / _` X X / / _ X '__| '_ X   / _ X|  _| | __| '_ X / _ X   | | / __/ _ X   X X/  X/ / | |_  / _` | '__/ _` |"
+        sleep(0.25)
+        puts "   | |___| (_| |X V /  __/ |  | | | | | (_) | |   | |_| | | |  __/  _| || (_|  __/    X  /X  /  | |/ / (_| | | | (_| |"
+        sleep(0.25)
+        puts "    X_____X__,_| X_/ X___|_|  |_| |_|  X___/|_|    X__|_| |_|X___| |_____X___X___|     X/  X/   |_/___X__,_|_|  X__,_|"
+        puts " "
+        puts " "
+        puts " "
+        puts " "
+        puts "                                            /'X                                                         "
+        sleep(0.25)
+        puts "                                           /   X              /'X       _                               "
+        sleep(0.25)
+        puts "                                      /'.,/     X_         .,'   X     / X_                             "
+        sleep(0.25)
+        puts "                                     /            X      _/       X_  /    X     _                      "
+        sleep(0.25)
+        puts "                            X__,.   /              X    /           X/.,   _|  _/ X                     "
+        sleep(0.25)
+        puts "                           /     X_/                X  /',.,''X      X_ X_/  X/    X                    "
+        sleep(0.25)
+        puts "                           |                      _  X/   /    ',../',.X    _/      X                   "
+        sleep(0.25)
+        puts "                          /         /           _XmX  X  /    |         X  /.,/'X   _X                  "
+        sleep(0.25)
+        puts "                         /        _/           XMMmmX  X_     |          X/      X_/  X                 "
+        sleep(0.25)
+        puts "                        /        /      X     XMMMMmmX   X__   X          X_       X   X_               "
+        sleep(0.25)
+        puts "                       /        /        X   XMMMMMMmX      X   X           X       X    X              "
+        sleep(0.25)
+        puts "                     _/        /          X  XMMMMMMmmX      X___            X_      X_   X             "
+        sleep(0.25)
+        puts "                    /         /            XXMMMMMMMMmmX____.'  /X_            X       X   X_           "
+        sleep(0.25)
+        puts "   ________________/_________/             /'.,___________...,,'   X            X   X        X________________________ "
+        puts " "
+        puts " "
+        sleep(0.25)
+        puts " "
+        puts " "
+        puts "Welcome to Cavern of the Ice Wizard!!  Would you like instructions?"
     end
     # A User can create and save a Player
     def create_new_player
